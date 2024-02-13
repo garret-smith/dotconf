71872991e371440f3d85c16835aabbfde1dde94f
Garret Smith <garrets@dropbox.com>
2024-02-12 16:36:14 -0800

    Sofle tweaks

--- keyboards/sofle/keymaps/gsmith/config.h
+++ keyboards/sofle/keymaps/gsmith/config.h
@@ -17,27 +17,33 @@
 
 #pragma once
 
-/* By default left side is selected as master, 
+/* By default left side is selected as master,
 see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options. */
+
+#define CUSTOM_LAYER_READ //if you remove this it causes issues - needs better guarding
+#define SPLIT_USB_DETECT
+
+#ifdef TAPPING_TERM
+  #undef TAPPING_TERM
+  #define TAPPING_TERM 200
+#endif
+#define PERMISSIVE_HOLD
 
 // Add RGB underglow and top facing lighting
 #define WS2812_DI_PIN D3
 #define RGBLIGHT_SLEEP
 #define RGBLIGHT_SPLIT
-#define RGBLED_NUM 12
-#define RGBLED_SPLIT { 6, 6 }
-#    define RGBLIGHT_EFFECT_BREATHING
-#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
-//#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-//#    define RGBLIGHT_EFFECT_SNAKE
-#    define RGBLIGHT_EFFECT_KNIGHT
-//#    define RGBLIGHT_EFFECT_CHRISTMAS
-//#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
-//#    define RGBLIGHT_EFFECT_RGB_TEST
-//#    define RGBLIGHT_EFFECT_ALTERNATING
-//#    define RGBLIGHT_EFFECT_TWINKLE
-#    define RGBLIGHT_LIMIT_VAL 120
-#    define RGBLIGHT_HUE_STEP 10
-#    define RGBLIGHT_SAT_STEP 17
-#    define RGBLIGHT_VAL_STEP 17
+//#define RGBLIGHT_LAYERS
+
+#undef RGBLED_NUM
+#define RGBLED_NUM 70    // Number of LEDs
+#define RGBLED_SPLIT { 35, 35 } // Number of LEDs on each half
+
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#define RGBLIGHT_EFFECT_KNIGHT
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_LIMIT_VAL 120
+#define RGBLIGHT_HUE_STEP 10
+#define RGBLIGHT_SAT_STEP 17
+#define RGBLIGHT_VAL_STEP 17
